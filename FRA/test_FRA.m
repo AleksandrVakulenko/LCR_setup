18d551dd4b0befb820b4ac3c9dbc9a4b9b082b01
Aleksandr Vakulenko <sasha705@mail.ru>
2025-04-04 00:15:24 +0300

    update FRA test

--- FRA/test_FRA.m
+++ FRA/test_FRA.m
@@ -4,8 +4,10 @@
 % Test for FRA measurement
 
 % ----TODO----:
-% 1) do first run
-% 2) seve results func
+% 1) find error in date receive
+% 2) filter speed effect?
+% 3) sample ping
+% 4) auto-range
 % ------------
 
 clc
@@ -16,8 +18,11 @@ Freq_min = 1; % Hz
 Freq_max = 1000; % Hz
 Freq_num = 20;
 Voltage_gen = 1; % V
-Delta_limit = 50/1e6; % 1
-filename = "test_05_C.mat";
+Delta_limit = 50/1e6;
+filename = "test_06_R.mat";
+Read_mode = "old"; % "old" or "new"
+
+
 
 % DEV INIT
 SR860 = SR860_dev(4);
@@ -25,38 +30,28 @@ Ammeter = K6517b_dev(27);
 
 % MAIN
 try
-    SR860.set_advanced_filter("on");
-    SR860.set_sync_filter('on');
-    SR860.set_detector_phase(0);
-    SR860.set_expand(1, "XYR");
-    SR860.set_harm_num(1);
-    SR860.set_sync_src("INT");
-    SR860.set_voltage_input_range(1);
-    SR860.set_filter_slope("6 dB/oct");
-    SR860.configure_input("VOLT");
-    SR860.set_gen_config(0.001, 1e3);
+    SR860_set_common_profile(SR860);
+    SR860.set_filter_slope("24 dB/oct"); % FIXME: fast or slow?
     SR860.set_sensitivity(1, "voltage"); % FIXME: need auto-mode
     
-    Voltage_gen_rms = Voltage_gen/sqrt(2);
     Current_max = Voltage_gen/R_test;
-%     Current_max = 1e-7;
-    freq_list = 10.^linspace(log10(Freq_min), log10(Freq_max), Freq_num);
-    freq_list = flip(freq_list);
+    %Current_max = 1e-7;
 
     Ammeter.config("current");
-    Sense = Ammeter.set_sensitivity(Current_max*1.2, "current");
+    Sense = Ammeter.set_sensitivity(Current_max*1.1, "current");
     Ammeter.enable_feedback("enable");
-    adev_utils.Wait(3);
+
+    [freq_list, min_time] = freq_list_gen(Freq_min, Freq_max, Freq_num);
 
     Fig = FRA_plot(freq_list, 'I, A', 'Phase, °');
-
+    
+    Voltage_gen_rms = Voltage_gen/sqrt(2);
+    Time_arr = [];
     A_arr = [];
     P_arr = [];
     F_arr = [];
     Timer = tic;
     for i = 1:numel(freq_list)
-        time = toc(Timer);
-
         freq = freq_list(i);
         SR860.set_gen_config(Voltage_gen_rms, freq);
         Period = 1/freq;
@@ -67,14 +62,15 @@ try
         else
             SR860.set_time_constant(1.5*Period);
         end
-        % -----------------------------------------------
-        adev_utils.Wait(Period*1);
 
+        % -----------------------------------------------
+        adev_utils.Wait(Period*1, 'Wait one period');
         stable = false;
-        %pause(0.1)
-        [R_old, Phase_old] = data_get_R_and_Phase_wrapper(SR860);
+        [R_old, Phase_old] = data_get_R_and_Phase_wrapper(SR860, Read_mode);
+        Stable_timeout = Period;
+        Stable_start_time = toc(Timer);
         while ~stable
-            [Amp, Phase] = data_get_R_and_Phase_wrapper(SR860);
+            [Amp, Phase] = data_get_R_and_Phase_wrapper(SR860, Read_mode);
             Delta_R = (Amp - R_old)/Amp;
             Delta_Phase = (Phase - Phase_old)/Phase;
             R_old = Amp;
@@ -84,13 +80,19 @@ try
                 stable = true;
             end
             DISP_DELTA(Delta, Delta_limit, "ppm");
-            %pause(0.05)
+            if (toc(Timer) - Stable_start_time) > Stable_timeout
+                stable = true;
+            end
+%             pause(0.05)
         end
         % -----------------------------------------------
+
         [Amp, Phase] = SR860.data_get_R_and_Phase;
+        time = toc(Timer);
 
         Amp = Amp*Sense*sqrt(2);
 
+        Time_arr = [Time_arr time];
         A_arr = [A_arr Amp];
         P_arr = [P_arr Phase];
         F_arr = [F_arr freq];
@@ -109,24 +111,37 @@ catch ERR
 end 
 
 disp("Finished without errors")
-disp(['Time passed = ' num2str(time) ' s']);
+disp(['Time passed = ' num2str(Time_arr(end)) ' s']);
+disp(['Minimum time = ' num2str(min_time) ' s']);
+
 
 Ammeter.enable_feedback("disable");
 delete(SR860);
 delete(Ammeter);
 
-save(filename, "A_arr", "P_arr", "F_arr", "Sense")
+save(filename, "A_arr", "P_arr", "F_arr", "Time_arr", "Sense")
 
 
 
-function [Amp, Phase] = data_get_R_and_Phase_wrapper(SR860)
+
+%-------------------------------------------------------------------------------
+
+function [Amp, Phase] = data_get_R_and_Phase_wrapper(SR860, mode)
+arguments
+    SR860
+    mode {mustBeMember(mode, ["old", "new"])}
+end
     OK = false;
     while ~OK
         try
             [Amp, Phase] = SR860.data_get_R_and_Phase;
             OK = true;
         catch
-            try_to_read_error(SR860);
+            if mode == "new"
+                try_to_read_error(SR860);
+            else
+                disp("CATCH ERROR !")
+            end
         end
     end
 end
@@ -140,4 +155,23 @@ function try_to_read_error(SR860)
 %     pause(5)
 end
 
+function [freq_list, min_time] = freq_list_gen(Freq_min, Freq_max, Freq_num)
+    freq_list = 10.^linspace(log10(Freq_min), log10(Freq_max), Freq_num);
+    freq_list = flip(freq_list);
+    periods = 1./freq_list;
+    min_time = sum(periods);
+end
+
+
+function SR860_set_common_profile(SR860)
+    SR860.configure_input("VOLT");
+    SR860.set_advanced_filter("on");
+    SR860.set_sync_filter('on');
+    SR860.set_expand(1, "XYR");
+    SR860.set_sync_src("INT");
+    SR860.set_harm_num(1);
+    SR860.set_voltage_input_range(1);
+    SR860.set_detector_phase(180); % NOTE: inv for K6517b
+    SR860.set_gen_config(100e-6, 1e3); % NOTE: off
+end
 
