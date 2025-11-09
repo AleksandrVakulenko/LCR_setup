1c212cd132239ba58f462524048753dc48175d1d
Aleksandr Vakulenko <sasha705@mail.ru>
2025-11-09 17:50:05 +0300

    Aster calibration and small updates

---
+++ FRA/test_FRA3.m
@@ -0,0 +1,173 @@
+% Date: 2025.04.23
+%
+% ----INFO----:
+% FRA measurement of calibration resistors
+%
+% ----TODO----:
+%  1) Freq list generator
+%  2) time predictor auto-update
+%  3) Add more settings to stable_test
+%  4) 
+%  5) Sample info struct
+%  6) Add Harm measuring
+%  7)
+%  8) Add lock-in I-V converter
+%  9) Test lock-in I-V converter
+% 10) If lock-in I-V it is the same dev handle ?!
+
+% ----TODO (in progress)----:
+%  1) sample ping
+%  2) auto-range
+%  3) Replace correction by interp1
+%  4) FRA settings struct
+% ---------------------------
+
+% Aster calibration table
+% 01) 3L  R = 1000    V = 1.0   (10.0%)
+% 02) 4L  R = 1000    V = 0.5   (50.0%)
+% 03) 5H  R = 1000    V = 0.05  (50.0%)
+% 04) 6H  R = 1000000 V = 1.0   (10.0%)
+% 05) 7H  R = 1000000 V = 0.5   (50.0%)
+% 06) 8H  R = 1000000 V = 0.05  (50.0%)
+% 07) 9H  R = 1000000 V = 0.005 (50.0%)
+
+Calibration_current = [2e-3
+                       250e-6
+                       2.5e-6
+                       20e-9
+                       250e-12
+                       2.5e-12];
+
+Calibration_voltage = [2.0
+                       0.25
+                       0.0025
+                       2.0
+                       0.025
+                       0.000250
+                       ];
+
+Calibration_res = [1000
+                   1000
+                   1000
+                   100e6
+                   100e6
+                   100e6
+                   ];
+
+
+Calibration_repeat = 1;
+
+%%
+% error('UPDATE folder')
+main_save_folder = '..\..\test_results_2025_11_10\';
+mkdir(main_save_folder);
+%%
+clc
+
+Delta_limit = 50e-6;
+save_files_flag = true;
+save_stable_data = false;
+
+for Cal_N = [1, 2, 3] % [1, 2, 3] OR [4, 5, 6, 7]
+    R_test = Calibration_res(Cal_N); % Ohm / 1e3, 1e6, 100e6, 1e9
+    Sense_Level = Calibration_sense_level(Cal_N); % 3(L), 4()L), 5(H), 6(H), 7(H), 8(H), 9(H)
+    Voltage_gen = Calibration_voltage(Cal_N); % V
+
+    local_save_folder = ['Calibration_N_' num2str(Cal_N, "%02u") '\'];
+    save_file_folder = [main_save_folder local_save_folder];
+    mkdir(save_file_folder);
+
+    for Cal_rep_i = 1:Calibration_repeat
+
+        experimental_setup = Aster_calibration();
+
+        if save_files_flag
+            filename = [save_file_folder 'cfile_' ...
+                num2str(Cal_N, '%02u') '_' ...
+                num2str(Cal_rep_i, '%02u') ...
+                '_R.mat'];
+        end
+
+        freq_list = experimental_setup.freq_list;
+        Phase_inv = experimental_setup.I2V_converter.phase_inv;
+        Divider_value = experimental_setup.divider_value;
+        % Voltage_gen = experimental_setup.sample_voltage; % V
+
+        % DEV INIT
+        [Lockin, Ammeter] = init_devices(experimental_setup);
+
+        % MAIN -----------------------------------------------------------------
+        Main_error = [];
+        try
+            SR860_set_common_profile(Lockin, Phase_inv); %FIXME: refactor
+            %     Current_max = Voltage_gen/R_test;
+            Sense_V2C = Ammeter.set_sensitivity(Sense_Level);
+            find_best_sense(Lockin, Voltage_gen); % FIXME: undone sample ping
+            Fig = FRA_plot(freq_list, 'I, A', 'Phase, °');
+
+            Time_arr = zeros(size(freq_list)); % FIXME: create time monitor
+            Data = FRA_data('I, [A]'); % FIXME: refactor this class
+            Timer = tic;
+            for i = 1:numel(freq_list)
+                freq = freq_list(i);
+                disp(' ')
+                disp(['freq = ' num2str(freq) ' Hz'])
+
+                if ~save_stable_data
+                    save_pack = [];
+                else
+                    save_pack = struct('comment', "real run", ...
+                        'freq_list', freq_list, 'freq', freq, 'i', i);
+                end
+
+                % FIXME: Lock_in_measure is bad!
+                [Amp, Phase] = Lock_in_measure(Lockin, ...
+                    Voltage_gen, freq, Delta_limit, "fine", save_pack);
+
+                Amp = Amp*Sense_V2C*sqrt(2)/Divider_value;
+
+                % FIXME: add calibration correction here
+
+                % FIXME: add impedance calc
+                %  Amp = Voltage_gen./Amp;
+
+                time = toc(Timer);
+                Time_arr(i) = time;
+                Data.add(freq, "R", Amp, "Phi", Phase);
+
+                Data_corr = Data.correction();
+
+                Fig.replace_FRA_data(Data);
+            end
+            % END MAIN --------------------------------------------------------------------
+        catch Main_error
+
+        end
+
+        Lockin.terminate();
+        delete(Lockin);
+
+        Ammeter.terminate();
+        delete(Ammeter);
+
+
+        if isempty(Main_error)
+            disp("Finished without errors")
+            Time_passed = Time_arr(end);
+            time_predictor_disp(freq_list, Time_passed)
+
+            if save_files_flag % FIXME: need refactor
+                close(gcf);
+                [F_arr, A_arr, P_arr] = Data.RPhi;
+                %         save(filename, "A_arr", "P_arr", "F_arr", "Time_arr", "Sense_V2C")
+                save(filename); % NOTE: SAVE ALL DATA!
+            end
+
+        else
+            rethrow(Main_error);
+        end
+
+
+    end % Calibration loops end
+end
+
--- Sample_simulation2.m
+++ Sample_simulation2.m
@@ -1,19 +1,66 @@
 
 
+
+Res = [100 10e3 1e6 100e6 10e9 1e12];
+freq_red = [500e3 300e3 30e3 300 1 0.05];
+freq_green = [250e3 100e3 10e3 100 1 0.05];
+
+Current = 5./Res;
+
+hold on
+plot(log10(Current), log10(freq_red), '-r', 'LineWidth', 3)
+plot(log10(Current), log10(freq_green), '-g', 'LineWidth', 3)
+% plot(log10(Current/100), log10(freq), '-r', 'LineWidth', 3)
+% plot(freq, Res, '-r', 'LineWidth', 3)
+
+% set(gca, 'xscale', 'log')
+% set(gca, 'yscale', 'log')
+
+%%
+
+Res = [100 10e3 1e6 100e6 10e9 1e12];
+freq_red = [500e3 300e3 30e3 300 1 0.05];
+freq_green = [250e3 100e3 10e3 100 1 0.05];
+freq = freq_green;
+
+Current_max = 5./Res;
+Current_min = Current_max/256;
+freq2 = [];
+freq2(1:2:numel(freq)*2) = freq;
+freq2(2:2:numel(freq)*2) = freq;
+freq = freq2;
+Current2 = [];
+Current2(1:2:numel(Current_max)*2) = Current_max;
+Current2(2:2:numel(Current_max)*2) = Current_min;
+Current = Current2;
+
+hold on
+plot(log10(Current), log10(freq), '-r', 'LineWidth', 3)
+
+%%
+
+Current_list = linspace(-15, 0, 101);
+Freq_list = 10.^linspace(log10(0.001), log10(500e3), 100);
+i = 20;
+Full_map = zeros(100, 100);
+
 clc
 
+for i = 1:numel(Freq_list)
+    i
 N = 50000;
 Thickness = Thickness_rand(N) * 1e-6; % m
 Diameter = Diameter_rand(N) * 1e-3; % m
 Eps = Eps_rand(N);
-freq = 10.^linspace(log10(0.001), log10(10e3), 10); % Hz
+freq = Freq_list(i); % Hz
 % freq = 10.^linspace(log10(80), log10(500e3), 10); % Hz
 % freq = 10;
 V = 1.0;
 
 
 S = pi*(Diameter/2).^2; % m^2
-Cap = 8.85e-12*Eps.*S./Thickness; % F
+% Cap = 8.85e-12*Eps.*S./Thickness; % F
+Cap = [1e-12, 10e-12, 30e-12, 100e-12, 1e-9, 2e-9, 5e-9, 10e-9, 20e-9];
 R = 1./(2*pi*Cap'*freq);
 R = reshape(R, 1, numel(R));
 I = V./R;
@@ -22,37 +69,27 @@ I = V./R;
 I = reshape(I, 1, numel(I));
 I_log = log10(I);
 
-%
-clc
-
 fig = figure('Visible', 'off');
-H = histogram(I_log, 'Normalization', 'pdf');
+H = histogram(I_log, Current_list, 'Normalization', 'pdf');
+% H = histogram(I_log, 'Normalization', 'pdf');
 Edges = H.BinEdges;
 I_log2 = (Edges(1:end-1) + Edges(2:end))/2;
 PDF = H.Values;
 delete(fig)
 
-Min = prctile(I_log, 0.5);
-P10 = prctile(I_log, 5);
-P90 = prctile(I_log, 95);
-Max = prctile(I_log, 99.5);
+Full_map(i, :) = PDF;
+end
 
+imagesc(Current_list, log10(Freq_list), Full_map)
+set(gca,'YDir','normal')
 
+%%
 figure('Position', [400  375  815  530])
 I_log2 = 10.^I_log2;
 plot(I_log2, PDF, 'Marker', 'none', 'LineWidth', 2.0)
-% xline(10.^P10, '--b', 'LineWidth', 1)
-% xline(10.^P90, '--b', 'LineWidth', 1)
 
-xline(30e-3, '-r', 'LineWidth', 1)
-xline(500e-6, '-r', 'LineWidth', 1)
-xline(5e-6, '-r', 'LineWidth', 1)
-xline(50e-9, '-r', 'LineWidth', 1)
-xline(500e-12, '-r', 'LineWidth', 1)
-xline(5e-12, '-r', 'LineWidth', 1)
 
-xline(7e-12, '--k', 'LineWidth', 2)
-xline(25e-15, '--k', 'LineWidth', 2)
+
 
 xlim([1e-15 1])
 xlabel('I, A')
@@ -65,27 +102,6 @@ set_axis_ticks(gca, "auto", "x")
 
 
 
-
-%
-
-% I_array = [1e-2, ...
-%            1e-3, 1e-4, 1e-5, ...
-%            1e-6, 1e-7, 1e-8, ...
-%            1e-9, 1e-10, 1e-11, ...
-%            1e-12, 1e-13];
-
-
-V_array = [2, 1, 0.5, 0.05, 0.005, 500e-6, 50e-6];
-
-R_array = [100, 1e3, 1e6, 100e6, 1e9];
-
-
-hold on
-
-for i = 1:numel(R_array)
-    I_array = V_array/R_array(i);
-    plot(I_array, (i+1)*ones(size(I_array))/300, '.-b', 'MarkerSize', 12)
-end
 
 
 
@@ -190,6 +206,10 @@ Eps = Dist_rand*1000;
 % plot(x, CDF)
 % histogram(Dist_rand)
 end
+
+
+
+
 
 
 
