20273a58fc049e4318788a87597f89978b635d6b
Aleksandr Vakulenko <sasha705@mail.ru>
2026-01-12 09:54:05 +0300

    update interp_FRA_data()

--- FRA/test_FRA4.m
+++ FRA/test_FRA4.m
@@ -279,18 +279,15 @@ Data_out = Data_to_corr * Correction_data_part;
 end
 
 
-function Data_out = interp_FRA_data(Data, Target)
+function Data_out = interp_FRA_data(Data_in, Data_target)
 
-Freq_tg = Target.freq;
+[Freq_tg, ~, ~] = Data_target.RPhi();
+[Freq_in, R_in, Phi_in] = Data_in.RPhi();
 
-Freq = Data.freq;
-X = Data.X;
-Y = Data.Y;
+R_new = interp1(Freq_in, R_in, Freq_tg);
+Phi_new = interp1(Freq_in, Phi_in, Freq_tg);
 
-X_out = interp1(Freq, X, Freq_tg);
-Y_out = interp1(Freq, Y, Freq_tg);
-
-Data_out = FRA_data(Data.unit, Freq_tg, "X", X_out, "Y", Y_out);
+Data_out = FRA_data(Data_in.unit, Freq_tg, "R", R_new, "Phi", Phi_new);
 
 end
 
